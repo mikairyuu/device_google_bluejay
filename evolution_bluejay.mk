4176340f343c066b0f77fee78567a96edea1cc93
mikairyuu <miva200267@gmail.com>
2023-01-19 15:48:34 +1000

    Init evolution

--- evolution_bluejay.mk
+++ evolution_bluejay.mk
@@ -1,15 +1,26 @@
 # Inherit some common stuff.
-$(call inherit-product, vendor/calyx/config/common.mk)
+$(call inherit-product, vendor/evolution/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/google/gs101/calyx_common.mk)
-$(call inherit-product, device/google/bluejay/device-calyx.mk)
+$(call inherit-product, device/google/gs101/evolution_common.mk)
+$(call inherit-product, device/google/bluejay/device-evolution.mk)
 $(call inherit-product, device/google/bluejay/aosp_bluejay.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := calyx_bluejay
+PRODUCT_NAME := evolution_bluejay
 PRODUCT_MODEL := Pixel 6a
 PRODUCT_BRAND := google
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
